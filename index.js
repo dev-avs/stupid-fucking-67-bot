b7aff6c282bbb105faad5411a077fe8048c76228
avs <98419107+dev-avs@users.noreply.github.com>
2026-02-10 20:50:41 -0800

    Initialize Discord bot with message handling

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  await message.channel.send("67");
+});
+
+client.login(process.env.TOKEN);
